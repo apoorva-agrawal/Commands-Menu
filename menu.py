f5c46f318664c1e72feb06c97a49ee7a698ed2a9
Apoorva Agrawal <52560781+apoorva-agrawal@users.noreply.github.com>
2020-08-22 19:55:43 +0530

    New commands added

--- menu.py
+++ menu.py
@@ -1,43 +1,60 @@
 def my_menu():
     import subprocess as sp
     from termcolor import colored
+    history = []
+
     while True:
         print("****** Command Menu ******\n")
         print('Press 1: to open firefox browser')
         print('Press 2: to open notepad')
         print('Press 3: to display date')
         print('Press 4: to display calender')
-        print('Press 5: exit\n\n')
+        print('Press 5: to display history')
+        print('Press 6: exit\n\n')
         print('Enter your choice: ', end = '')
         param = input()
-        print("result: ")
 
-        if int(param) ==1:
-            data = sp.getstatusoutput("firefox")
+
+        if int(param) <=6:
+
+            if int(param) ==1:
+                data = sp.getstatusoutput("firefox")
+                history.append('firefox')
+
+            elif int(param) ==2:
+                data = sp.getstatusoutput("notepad")
+                history.append('notepad')
+
+            elif int(param) ==3:
+                data = sp.getstatusoutput("date")
+                history.append('date')
+
+            elif int(param) ==4:
+                data = sp.getstatusoutput("cal")
+                history.append('cal')
+
+            elif int(param) ==5:
+                print(colored('HISTORY','blue'))
+                print(colored(history,'blue'))
+                history.append('history')
+
+            else:
+                break
+
+            print('status:')
+            if int(param) <=4:
+                if data[0]==0:
+                    print(colored('SUCCESS\n','green'))
+                else:
+                    print(colored('FAILURE\n','red'))
+
+            print('result')
             print(data)
             print('\n')
-            
 
-        elif int(param) ==2:
-            data = sp.getstatusoutput("notepad")
-            print(data)
-            print('\n')
-             
-
-        elif int(param) ==3:
-            data = sp.getstatusoutput("date")
-            print(data)
-            print('\n')
-             
-
-        elif int(param) ==4:
-            data = sp.getstatusoutput("cal")
-            print(data)
-            print('\n')
- 
         else:
-            break
-        print(colored('SUCCESS\n','green'))if data[0]==0 else print(colored('FAILURE\n','red'))
+            print(colored('INVALID COMMAND! Kindly enter right command \n','red'))
+       
     exited = print(colored('EXITED','blue'))
     return exited
   
